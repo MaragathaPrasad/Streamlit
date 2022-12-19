e262e314181f2e964562a6297fc444a2cebc451d
MaragathaPrasad <108782756+MaragathaPrasad@users.noreply.github.com>
2022-12-19 11:09:21 +0530

    Add files via upload

---
+++ New.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 19 10:08:25 2022
+
+@author: prasad
+"""
+
+import streamlit as st
+
+st.markdown("IS EVERYTHING OKAY!! This text is :red[colored red] and this is **:blue[colored]** and bold")
